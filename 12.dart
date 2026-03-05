e36a9f0915fb95aa9e0a93162d04b61c0e30deba
Naseem <muhammednaseemtk000@gmail.com>
2026-03-05 10:06:19 +0530

    12

--- 12.dart
+++ 12.dart
@@ -1,35 +1,35 @@
-// void main() {
-//   List<int> days = [1, 2, 3, 4, 5, 6, 7];
+void main() {
+  List<int> days = [1, 2, 3, 4, 5, 6, 7];
 
-//   for (int i = 1; i <= 7; i++) {
-//     switch (days[i]) {
-//       case 1:
-//         print("$i:Monday");
-//         break;
+  for (int i = 1; i <= 7; i++) {
+    switch (days[i]) {
+      case 1:
+        print("$i:Monday");
+        break;
 
-//       case 2:
-//         print("$i:Tuesday");
-//         break;
+      case 2:
+        print("$i:Tuesday");
+        break;
 
-//       case 3:
-//         print("$i:Wednesday");
-//         break;
+      case 3:
+        print("$i:Wednesday");
+        break;
 
-//       case 4:
-//         print("$i:Thursday");
-//         break;
+      case 4:
+        print("$i:Thursday");
+        break;
 
-//       case 5:
-//         print("$i:Friday");
-//         break;
+      case 5:
+        print("$i:Friday");
+        break;
 
-//       case 6:
-//         print("$i:Saturday");
-//         break;
+      case 6:
+        print("$i:Saturday");
+        break;
 
-//       case 7:
-//         print("$i:Sunday");
-//         break;
-//     }
-//   }
-// }
+      case 7:
+        print("$i:Sunday");
+        break;
+    }
+  }
+}
